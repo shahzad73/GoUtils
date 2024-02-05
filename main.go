a1fb2c3fef8dc4f77712596357b92a093525887e
Shahzad Aslam <sa@digishares.io>
2024-02-05 23:02:40 +0500

    test modules

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/shahzad73/GoUtils/utils"
+)
+
+func main() {
+
+	fmt.Println("Hello, Modules!")
+
+	utils.PrintHello()
+
+}
---
+++ utils/utils.go
@@ -0,0 +1,7 @@
+package utils
+
+import "fmt"
+
+func PrintHello() {
+	fmt.Println("Hello, Modules! This is mypackage speaking! 123")
+}
